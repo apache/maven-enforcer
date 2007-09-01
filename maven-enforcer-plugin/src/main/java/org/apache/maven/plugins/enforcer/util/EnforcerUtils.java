c69f04d06969a8eea6173a7119280cafafd67bf8
Brian E Fox <brianf@apache.org>
2007-09-01 02:57:05 +0000

    more refactor
    git-svn-id: https://svn.apache.org/repos/asf/maven/enforcer/trunk@571672 13f79535-47bb-0310-9956-ffa450edef68

--- maven-enforcer-plugin/src/main/java/org/apache/maven/plugins/enforcer/util/EnforcerUtils.java
+++ maven-enforcer-plugin/src/main/java/org/apache/maven/plugins/enforcer/util/EnforcerUtils.java
@@ -12,28 +12,5 @@ import org.apache.maven.artifact.versioning.VersionRange;
  */
 public class EnforcerUtils
 {
-    public static synchronized boolean containsVersion( VersionRange allowedRange, ArtifactVersion theVersion )
-    {
-        boolean matched = false;
-        ArtifactVersion recommendedVersion = allowedRange.getRecommendedVersion();
-        if ( recommendedVersion == null )
-        {
 
-            for ( Iterator i = allowedRange.getRestrictions().iterator(); i.hasNext() && !matched; )
-            {
-                Restriction restriction = (Restriction) i.next();
-                if ( restriction.containsVersion( theVersion ) )
-                {
-                    matched = true;
-                }
-            }
-        }
-        else
-        {
-            // only singular versions ever have a recommendedVersion
-            int compareTo = recommendedVersion.compareTo( theVersion );
-            matched = ( compareTo <= 0 );
-        }
-        return matched;
-    }
 }
