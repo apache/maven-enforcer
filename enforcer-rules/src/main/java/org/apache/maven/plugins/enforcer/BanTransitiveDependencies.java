bdc5c66050218a2dcbf8760080055ff2658f3d75
jsenko <jsenko@redhat.com>
2012-08-24 13:36:10 +0200

    Created BanTransitiveDependencies enforcer rule

---
+++ enforcer-rules/src/main/java/org/apache/maven/plugins/enforcer/BanTransitiveDependencies.java
@@ -0,0 +1,175 @@
+package org.apache.maven.plugins.enforcer;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.apache.maven.plugins.enforcer.utils.ArtifactMatcher;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
+import org.apache.maven.shared.dependency.graph.internal.DefaultDependencyGraphBuilder;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
+
+/**
+ * This rule bans all transitive dependencies.
+ * There is a configuration option to exclude certain artifacts
+ * from being checked (similar to BannedDependencies).
+ * 
+ * @author Jakub Senko
+ */
+public class BanTransitiveDependencies extends AbstractNonCacheableEnforcerRule implements EnforcerRule
+{
+	
+	private EnforcerRuleHelper helper;
+	
+    /**
+     * Specify the dependencies that will be ignored.
+     * This can be a list of artifacts in the format <code>groupId[:artifactId][:version][:type][:scope]</code>.
+     * Wildcard '*' can be used to in place of specific section
+     * (ie group:*:1.0 will match both 'group:artifact:1.0' and 'group:anotherArtifact:1.0') <br>
+     * You can override this patterns by using includes.
+     * Invalid and empty patterns will be ignored.
+     */
+	private List<String> excludes;
+	
+    /**
+     * Specify the dependencies that will be checked.
+     * These are exceptions to excludes intended for more convenient and finer settings.
+     * This can be a list of artifacts in the format <code>groupId[:artifactId][:version][:type][:scope]</code>.
+     * Wildcard '*' can be used to in place of specific section
+     * (ie group:*:1.0 will match both 'group:artifact:1.0' and 'group:anotherArtifact:1.0') <br>
+     * Invalid and empty patterns will be ignored.
+     */
+	private List<String> includes;
+
+	/**
+	 * Searches dependency tree recursively for transitive dependencies
+	 * that are not excluded, while generating nice info message
+	 * along the way.
+	 */
+	private static boolean searchTree(DependencyNode node, int level, ArtifactMatcher excludes, StringBuilder message)
+	{
+		
+		List<DependencyNode> children = node.getChildren();
+		
+		/*
+		 * if the node is deeper than direct dependency
+		 * and is empty, it is transitive.
+		 * if its not empty it may contain only excluded dependencies
+		 * and does not have to cause failure
+		 */
+		boolean hasTransitiveDependencies = level > 1 && children.size() == 0;
+		
+		boolean excluded = false;
+		
+		/*
+		 * holds recursive message from children,
+		 * will be appended to current message
+		 * if this node has any transitive descendants
+		 * if message is null, don't generate recursive message.
+		 */
+		StringBuilder messageFromChildren =  message == null ? null : new StringBuilder();
+		
+		if(excludes.match(node.getArtifact())) 
+		{
+			// is excluded, we don't care about descendants
+			excluded = true;
+			hasTransitiveDependencies = false;
+		}
+		else
+		{
+			for(DependencyNode childNode: children)
+			{
+				/*
+				 * if any of the children has transitive d.
+				 * so does the parent
+				 */
+				hasTransitiveDependencies = (searchTree(childNode, level + 1, excludes, messageFromChildren)
+						|| hasTransitiveDependencies);
+			}
+		}
+		
+		
+		if((excluded || hasTransitiveDependencies) && message != null) // then generate message
+		{
+			for(int i = 0; i < level; i++) message.append("   ");
+			
+			message.append(node.getArtifact());
+		
+			if(excluded) 
+			{
+				message.append(" [excluded]\n");
+			}
+		
+			if(hasTransitiveDependencies)
+			{
+				if(level == 1) message.append(" has transitive dependencies:");
+		
+				message.append("\n").append(messageFromChildren);
+			}
+		}
+		
+		return hasTransitiveDependencies;
+	}
+	
+
+	public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException
+	{
+		this.helper = helper;
+		
+		if(excludes == null) excludes = Collections.emptyList();
+		if(includes == null) includes = Collections.emptyList();
+		
+		final ArtifactMatcher exclusions = new ArtifactMatcher(excludes, includes);	
+
+		DependencyNode rootNode = null;
+		
+		try
+		{
+			MavenProject project = (MavenProject) helper.evaluate("${project}");
+
+			rootNode = createDependencyGraphBuilder()
+					.buildDependencyGraph(project, null);
+		}
+		catch (Exception e)
+		{
+			throw new EnforcerRuleException("Error: Could not construct dependency tree.", e);
+		}
+
+		if(message == null)
+		{
+			StringBuilder generatedMessage = new StringBuilder();
+			
+			if(searchTree(rootNode, 0, exclusions, generatedMessage))
+			{				
+				throw new EnforcerRuleException(generatedMessage.toString());
+			}
+		}
+		else
+		{
+			if(searchTree(rootNode, 0, exclusions, null))
+			{				
+				throw new EnforcerRuleException(message);
+			}
+		}
+
+	}
+
+	private DependencyGraphBuilder createDependencyGraphBuilder()
+			throws ComponentLookupException
+	{
+		DefaultDependencyGraphBuilder builder = (DefaultDependencyGraphBuilder) helper
+			.getContainer()
+			.lookup(DependencyGraphBuilder.class.getCanonicalName(), "default");
+
+		builder.enableLogging(new ConsoleLogger(ConsoleLogger.LEVEL_DISABLED,
+			"DefaultDependencyGraphBuilder"));
+
+		return builder;
+	}
+
+}
