a99f285a7ba4585002dbea3ef72efbbdb2e85b17
Guillaume Boué <gboue@apache.org>
2016-09-20 20:40:57 +0000

    [MENFORCER-204] Add new rule: should be able to make sure that project artifact is a Snapshot
    Adding a RequireSnapshotVersion rule, which is the opposite of the RequireReleaseVersion rule.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/enforcer/trunk@1761637 13f79535-47bb-0310-9956-ffa450edef68

---
+++ enforcer-rules/src/main/java/org/apache/maven/plugins/enforcer/RequireSnapshotVersion.java
@@ -0,0 +1,92 @@
+package org.apache.maven.plugins.enforcer;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+
+/**
+ * This rule checks that the current project is not a release.
+ */
+public class RequireSnapshotVersion
+    extends AbstractNonCacheableEnforcerRule
+{
+
+    /**
+     * Allows this rule to fail when the parent is defined as a release.
+     */
+    private boolean failWhenParentIsRelease = true;
+
+    public void execute( EnforcerRuleHelper helper )
+        throws EnforcerRuleException
+    {
+
+        MavenProject project = getProject( helper );
+        Artifact artifact = project.getArtifact();
+
+        if ( !artifact.isSnapshot() )
+        {
+            String message = getMessage();
+            StringBuilder sb = new StringBuilder();
+            if ( message != null )
+            {
+                sb.append( message ).append( '\n' );
+            }
+            sb.append( "This project cannot be a release:" ).append( artifact.getId() );
+            throw new EnforcerRuleException( sb.toString() );
+        }
+        if ( failWhenParentIsRelease )
+        {
+            Artifact parentArtifact = project.getParentArtifact();
+            if ( parentArtifact != null && !parentArtifact.isSnapshot() )
+            {
+                throw new EnforcerRuleException( "Parent cannot be a release: " + parentArtifact.getId() );
+            }
+        }
+
+    }
+
+    private MavenProject getProject( EnforcerRuleHelper helper )
+        throws EnforcerRuleException
+    {
+        try
+        {
+            return (MavenProject) helper.evaluate( "${project}" );
+        }
+        catch ( ExpressionEvaluationException eee )
+        {
+            throw new EnforcerRuleException( "Unable to retrieve the MavenProject: ", eee );
+        }
+    }
+
+    public boolean isFailWhenParentIsRelease()
+    {
+        return failWhenParentIsRelease;
+    }
+
+    public void setFailWhenParentIsRelease( boolean failWhenParentIsRelease )
+    {
+        this.failWhenParentIsRelease = failWhenParentIsRelease;
+    }
+
+}
---
+++ enforcer-rules/src/test/java/org/apache/maven/plugins/enforcer/TestRequireSnapshotVersion.java
@@ -0,0 +1,101 @@
+package org.apache.maven.plugins.enforcer;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.apache.maven.plugin.testing.ArtifactStubFactory;
+import org.apache.maven.plugins.enforcer.utils.TestEnforcerRuleUtils;
+import org.apache.maven.project.MavenProject;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Test class for the RequireSnapshotVersion rule.
+ */
+public class TestRequireSnapshotVersion
+{
+
+    private MavenProject project;
+
+    private EnforcerRuleHelper helper;
+
+    private ArtifactStubFactory factory;
+
+    private RequireSnapshotVersion rule;
+
+    @Before
+    public void before()
+    {
+        project = new MockProject();
+        helper = EnforcerTestUtils.getHelper( project );
+        factory = new ArtifactStubFactory();
+        rule = new RequireSnapshotVersion();
+    }
+
+    @Test
+    public void testRequireSnapshot()
+        throws IOException
+    {
+        project.setArtifact( factory.getReleaseArtifact() );
+        TestEnforcerRuleUtils.execute( rule, helper, true );
+
+        project.setArtifact( factory.getSnapshotArtifact() );
+        TestEnforcerRuleUtils.execute( rule, helper, false );
+    }
+
+    @Test
+    public void testWithParentShouldFail()
+        throws IOException
+    {
+        project.setArtifact( factory.getSnapshotArtifact() );
+        rule.setFailWhenParentIsRelease( true );
+
+        MockProject parent = new MockProject();
+        parent.setArtifact( factory.getReleaseArtifact() );
+        project.setParent( parent );
+        TestEnforcerRuleUtils.execute( rule, helper, true );
+
+        parent = new MockProject();
+        parent.setArtifact( factory.getSnapshotArtifact() );
+        project.setParent( parent );
+        TestEnforcerRuleUtils.execute( rule, helper, false );
+    }
+
+    @Test
+    public void testWithParentShouldPass()
+        throws IOException
+    {
+        project.setArtifact( factory.getSnapshotArtifact() );
+        rule.setFailWhenParentIsRelease( false );
+
+        MockProject parent = new MockProject();
+        parent.setArtifact( factory.getReleaseArtifact() );
+        project.setParent( parent );
+        TestEnforcerRuleUtils.execute( rule, helper, false );
+
+        parent = new MockProject();
+        parent.setArtifact( factory.getSnapshotArtifact() );
+        project.setParent( parent );
+        TestEnforcerRuleUtils.execute( rule, helper, false );
+    }
+
+}
