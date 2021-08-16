b20f9114d67bd9eb7957b3e2c77ba99fcfe8efe1
Joe Barnett <jbarnett@tribedynamics.com>
2021-08-16 11:23:49 -0700

    [MENFORCER-394] Only look at shipped dependencies in DependencyConvergence
    This matches the 3.0.0-M3 behavior of the DependencyConvergence rule,
    including only non-optional compile and runtime dependencies.
    
    Closes ##102

--- enforcer-rules/src/main/java/org/apache/maven/plugins/enforcer/DependencyConvergence.java
+++ enforcer-rules/src/main/java/org/apache/maven/plugins/enforcer/DependencyConvergence.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
 import org.apache.maven.enforcer.rule.api.EnforcerRule;
@@ -82,7 +83,9 @@ public class DependencyConvergence
                 new DefaultProjectBuildingRequest( session.getProjectBuildingRequest() );
             buildingRequest.setProject( project );
             buildingRequest.setLocalRepository( repository );
-            ArtifactFilter filter = null; // we need to evaluate all scopes
+            ArtifactFilter filter = ( Artifact a ) -> ( "compile".equalsIgnoreCase( a.getScope () )
+                    || "runtime".equalsIgnoreCase( a.getScope () ) )
+                    && !a.isOptional();
 
             return dependencyCollectorBuilder.collectDependencyGraph( buildingRequest, filter );
         }
