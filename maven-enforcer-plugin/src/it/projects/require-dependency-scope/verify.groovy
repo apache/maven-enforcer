016d522739b96ad9f667283fd352e8632361d5f7
Konrad Windszus <kwin@apache.org>
2022-08-22 15:47:52 +0200

    fix IT after previous refactoring

--- maven-enforcer-plugin/src/it/projects/require-dependency-scope/verify.groovy
+++ maven-enforcer-plugin/src/it/projects/require-dependency-scope/verify.groovy
@@ -17,5 +17,5 @@
  * under the License.
  */
 File buildLog = new File(basedir, 'build.log')
-assert buildLog.text.contains('Found 1 missing dependency scope. Look at the warnings emitted above for the details.')
 assert buildLog.text.contains('[ERROR] Dependency org.apache.jackrabbit.vault:vault-cli:jar @ line 65, column 21 does not have an explicit scope defined!')
+assert buildLog.text.contains('Found 1 missing dependency scope. Look at the errors emitted above for the details.')
