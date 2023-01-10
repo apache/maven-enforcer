d442047e7fda2d62a42a4ccc2f324951556d1adc
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-01-10 22:21:02 +0100

    [MENFORCER-458] Move Require*Checksum to new API

--- enforcer-rules/src/main/java/org/apache/maven/enforcer/rules/checksum/RequireTextFileChecksum.java
+++ enforcer-rules/src/main/java/org/apache/maven/enforcer/rules/checksum/RequireTextFileChecksum.java
@@ -16,21 +16,24 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.maven.plugins.enforcer;
+package org.apache.maven.enforcer.rules.checksum;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.Objects;
 
 import org.apache.commons.io.input.ReaderInputStream;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleError;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
-import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
-import org.apache.maven.enforcer.rules.utils.NormalizeLineSeparatorReader;
-import org.apache.maven.enforcer.rules.utils.NormalizeLineSeparatorReader.LineSeparator;
-import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+import org.apache.maven.enforcer.rules.checksum.NormalizeLineSeparatorReader.LineSeparator;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Rule to validate a text file to match the specified checksum.
@@ -38,10 +41,19 @@ import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluatio
  * @author Konrad Windszus
  * @see RequireFileChecksum
  */
+@Named("requireTextFileChecksum")
 public class RequireTextFileChecksum extends RequireFileChecksum {
+
     private NormalizeLineSeparatorReader.LineSeparator normalizeLineSeparatorTo = LineSeparator.UNIX;
 
-    Charset encoding;
+    private Charset encoding;
+
+    private final MavenProject project;
+
+    @Inject
+    public RequireTextFileChecksum(MavenProject project) {
+        this.project = Objects.requireNonNull(project);
+    }
 
     public void setNormalizeLineSeparatorTo(NormalizeLineSeparatorReader.LineSeparator normalizeLineSeparatorTo) {
         this.normalizeLineSeparatorTo = normalizeLineSeparatorTo;
@@ -51,38 +63,47 @@ public class RequireTextFileChecksum extends RequireFileChecksum {
         this.encoding = Charset.forName(encoding);
     }
 
+    public Charset getEncoding() {
+        return encoding;
+    }
+
     @Override
-    public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
+    public void execute() throws EnforcerRuleException {
         // set defaults
         if (encoding == null) {
             // https://maven.apache.org/plugins/maven-resources-plugin/examples/encoding.html
-            try {
-                String encoding = (String) helper.evaluate("${project.build.sourceEncoding}");
-                if (StringUtils.isBlank(encoding)) {
-                    encoding = System.getProperty("file.encoding");
-                    helper.getLog()
-                            .warn("File encoding has not been set, using platform encoding " + encoding
-                                    + ". Build is platform dependent!");
-                }
-                this.encoding = Charset.forName(encoding);
-            } catch (ExpressionEvaluationException e) {
-                throw new EnforcerRuleException(
-                        "Unable to retrieve the project's build source encoding "
-                                + "(${project.build.sourceEncoding}): ",
-                        e);
+            String projectEncoding = project.getProperties().getProperty("project.build.sourceEncoding", null);
+            if (StringUtils.isBlank(projectEncoding)) {
+                projectEncoding = System.getProperty("file.encoding");
+                getLog().warn("File encoding has not been set, using platform encoding " + projectEncoding
+                        + ". Build is platform dependent! - https://maven.apache.org/general.html#encoding-warning");
             }
+            encoding = Charset.forName(projectEncoding);
         }
-        super.execute(helper);
+        super.execute();
     }
 
     @Override
     protected String calculateChecksum() throws EnforcerRuleException {
         try (Reader reader = new NormalizeLineSeparatorReader(
-                        Files.newBufferedReader(file.toPath(), encoding), normalizeLineSeparatorTo);
+                        Files.newBufferedReader(getFile().toPath(), encoding), normalizeLineSeparatorTo);
                 InputStream inputStream = new ReaderInputStream(reader, encoding)) {
             return super.calculateChecksum(inputStream);
         } catch (IOException e) {
-            throw new EnforcerRuleException("Unable to calculate checksum (with normalized line separators)", e);
+            throw new EnforcerRuleError("Unable to calculate checksum (with normalized line separators)", e);
         }
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "RequireFileChecksum[file=%s, checksum=%s, type=%s, encoding=%s, normalizeLineSeparatorTo=%s, nonexistentFileMessage=%s, level=%s]",
+                getFile(),
+                getChecksum(),
+                getType(),
+                encoding,
+                normalizeLineSeparatorTo,
+                getNonexistentFileMessage(),
+                getLevel());
     }
 }
